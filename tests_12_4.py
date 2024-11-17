a1bb996100c6d90ab57b38cbfc0103ea06815e49
kseniya1526 <kseniak505@gmail.com>
2024-11-17 22:35:26 +0700

    Add files via upload

---
+++ tests_12_4.py
@@ -0,0 +1,48 @@
+import TestCase as rnr
+import unittest
+import logging
+
+
+class RunnerTest(unittest.TestCase):
+    is_frozen = False
+
+    def test_walk(self):
+        try:
+            ole = rnr.Runner('Ole', -10)
+            logging.info(f"'test_walk' выполнен успешно")
+            i = 1
+            while i <= 10:
+                rnr.Runner.walk(ole)
+                i += 1
+            self.assertEqual(ole.distance, 100)
+        except:
+            logging.warning("Неверная скорость для Runner", exc_info=True)
+
+    def test_run(self):
+        try:
+            leo = rnr.Runner(23, 5)
+            logging.info(f"'test_run' выполнен успешно")
+            i = 1
+            while i <= 10:
+                rnr.Runner.run(leo)
+                i += 1
+            self.assertEqual(leo.distance, 100)
+        except:
+            logging.warning("Неверный тип данных для объекта Runner", exc_info=True)
+
+    # @unittest.skipUnless(is_frozen == False, 'Тесты в этом кейсе заморожены')
+    # def test_challenge(self):
+    #     alex = rnr.Runner('Alex', 10)
+    #     vlad = rnr.Runner('Vlad', 10)
+    #     i = 1
+    #     while i <= 10:
+    #         rnr.Runner.run(alex)
+    #         rnr.Runner.walk(vlad)
+    #         i += 1
+    #     self.assertNotEqual(alex.distance, vlad.distance)
+
+
+if __name__ == "__main__":
+    unittest.main()
+logging.basicConfig(level=logging.INFO, filemode='w', filename="runner_tests.log", encoding="UTF-8",
+                    format="%(levelname)s | %(message)s")
